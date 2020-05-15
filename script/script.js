f99f0467cfa2f41e3998e424d5cd22a5a36fed04
shariquereyaz <shariquereyaz@gmail.com>
2020-05-15 13:12:27 +0530

    .

--- script/script.js
+++ script/script.js
@@ -1,17 +1,17 @@
 
 
-$('body').on('click', function(){
-//   if( parseInt( $('.sidebar').css('width') ) > 0 ){
-    closeNav();
-//   }
-});
+// $('body').on('click', function(){
+// //   if( parseInt( $('.sidebar').css('width') ) > 0 ){
+//     closeNav();
+// //   }
+// });
 
-function closeNav() {
-    // document.getElementById("sidebar").style.width = "0";
-    // document.getElementsByClassName('sidebar')[0].style.visibility = 'hidden';
-    document.getElementsByClassName('sidebar')[0].style.display = 'none';
-}
+// function closeNav() {
+//     // document.getElementById("sidebar").style.width = "0";
+//     // document.getElementsByClassName('sidebar')[0].style.visibility = 'hidden';
+//     document.getElementsByClassName('sidebar')[0].style.display = 'none';
+// }
 
-function openNav(){
-    document.getElementsByClassName('sidebar')[0].style.display='block';
-}
+// function openNav(){
+//     document.getElementsByClassName('sidebar')[0].style.display='block';
+// }
